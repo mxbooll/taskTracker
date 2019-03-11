9e04de6e82996a3ac6d384b61a6eee8ee3a481ab
max <mxbooll@gmail.com>
2019-03-11 17:26:37 +0500

    3th commit

---
+++ tasktracker/Models/IStoryTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tasktracker.Models
+{
+    public interface IStoryTable
+    {
+        int ID { get; set; }
+        string Status { get; set; }
+        string Story { get; set; }
+        DateTime Time_enter { get; set; }
+    }
+}
